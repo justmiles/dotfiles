dbc13bc7b273f511efdca960a2b6afac8767e92e
Miles Maddox <j.milesmaddox@gmail.com>
2023-05-02 09:37:40 -0500

    remove install scripts

--- .chezmoiscripts/run_after_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export OS=$(cat /etc/os-release | grep "^ID=" | awk -F '=' '{print $2}')
-
-# Run the bootstrapping scripts
-find ~/.local/share/bootstrapping -type f | xargs -I % basename % | sort | grep -v "^_" | while read script; do
-    bash -c "~/.local/share/bootstrapping/$script"
-done
