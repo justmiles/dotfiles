57220f664c38bf6eaa373d301bcc1460da393c88
Miles Maddox <j.milesmaddox@gmail.com>
2023-07-13 12:38:43 -0500

    mint docker

--- dot_local/share/bootstrapping/executable_2_docker.sh
+++ dot_local/share/bootstrapping/executable_2_docker.sh
@@ -9,6 +9,24 @@ if [ "$OS" = "manjaro" ]; then
     yes | sudo pacman -S docker
 fi
 
+if [ "$OS" = "mint" ]; then
+
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+
+    echo \
+    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    "$(. /etc/os-release && echo "$UBUNTU_CODENAME")" stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update -o Dir::Etc::sourcelist=/etc/apt/sources.list.d/docker.list 
+
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+fi
+
 sudo systemctl start docker.service
 sudo systemctl enable docker.service
 
