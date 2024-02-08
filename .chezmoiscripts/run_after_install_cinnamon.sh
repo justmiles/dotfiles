f24c7182bf670ceb838d19e4a5463cfcaaea22d6
Miles Maddox <justmiles@users.noreply.github.com>
2024-02-08 15:56:51 -0600

    Update run_after_install_cinnamon.sh

--- .chezmoiscripts/run_after_install_cinnamon.sh
+++ .chezmoiscripts/run_after_install_cinnamon.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 # dconf dump /org/cinnamon/desktop/keybindings/ > ~/.config/cinnamon-desktop-keybindings.dconf
-
-dconf load /org/cinnamon/desktop/keybindings/ < ~/.config/cinnamon-desktop-keybindings.dconf
