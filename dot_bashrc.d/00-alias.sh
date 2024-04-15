66ff2c6688b95163751d3329f519618f53d8406d
Miles Maddox <j.milesmaddox@gmail.com>
2024-04-15 16:50:08 +0000

    feat: add new utility functions for file transfer and port generation
    - Add `transfer` alias for file transfer functionality
    - Add `port` function for generating random port numbers
    - Add `slack-kde-login` function for logging into Slack on KDE
    
    Signed-off-by: Miles Maddox <j.milesmaddox@gmail.com>

--- dot_bashrc.d/00-alias.sh
+++ dot_bashrc.d/00-alias.sh
@@ -21,3 +21,7 @@ slack-kde-login() {
 port(){
   shuf -i 2000-65000 -n 1
 }
+
+transfer() (if [ $# -eq 0 ]; then printf "No arguments specified.\nUsage:\n transfer <file|directory>\n ... | transfer <file_name>\n">&2; return 1; fi; file_name=$(basename "$1"); if [ -t 0 ]; then file="$1"; if [ ! -e "$file" ]; then echo "$file: No such file or directory">&2; return 1; fi; if [ -d "$file" ]; then cd "$file" || return 1; file_name="$file_name.zip"; set -- zip -r -q - .; else set -- cat "$file"; fi; else set -- cat; fi; url=$("$@" | curl --silent --show-error --progress-bar --upload-file "-" "https://transfer.justmiles.io/$file_name"); echo "$url"; )
+
+
